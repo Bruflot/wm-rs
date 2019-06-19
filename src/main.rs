8c164b355d210a0f7e7ba76f95a991a2e574da9d
Bruflot <git@bruflot.com>
2019-06-19 20:19:33 +0200

    Removed unused code

--- src/main.rs
+++ src/main.rs
@@ -12,22 +12,4 @@ fn main() {
     info!("Connected to X");
 
     wm.run();
-
-    // loop {
-    //     let event = wm.next_event();
-    //     let kind = event.kind();
-    //     info!("Event received: {:?}", kind);
-
-    //     match kind {
-    //         // A new window was created
-    //         // We rarely need to handle this as the application creates the
-    //         // window itself.
-    //         Events::CreateNotify => (),
-    //         Events::DestroyNotify => (),
-    //         Events::ConfigureRequest => {
-    //             info!("Configuring request");
-    //         }
-    //         _ => warn!("Ignored event"),
-    //     }
-    // }
 }
